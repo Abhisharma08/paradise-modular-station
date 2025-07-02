df2eba2850a4a803dbd1dfe44c0ada044f045c34
Abhisharma08 <aksharma223@gmail.com>
2025-07-02 15:51:05 +0530

    fixed build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { Inter, Playfair_Display } from 'next/font/google';
 import { cn } from '@/lib/utils';
-import GtmScript from '@/components/gtm-script';
+import Script from 'next/script';
 
 const fontBody = Inter({
   subsets: ['latin'],
@@ -31,6 +31,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      {GTM_ID && (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      )}
       <body
         className={cn(
           'font-body antialiased',
@@ -50,7 +61,6 @@ export default function RootLayout({
         )}
         {children}
         <Toaster />
-        <GtmScript />
       </body>
     </html>
   );
