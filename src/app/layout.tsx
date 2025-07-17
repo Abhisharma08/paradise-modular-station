99918472b44e41430912db0c434ae606e9acd3f2
Abhisharma08 <aksharma223@gmail.com>
2025-07-17 10:52:34 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import Script from 'next/script';
@@ -41,6 +42,7 @@ export default function RootLayout({
           }}
         />
         {children}
+        <Analytics />
         <Toaster />
       </body>
     </html>
